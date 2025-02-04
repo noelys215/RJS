7b32f32bcfeb0e1c15fcd2af0c3f346bd25a29d5
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-04 16:58:20 -0500

    chore: user input

--- alarm/src/Main.java
+++ alarm/src/Main.java
@@ -1,5 +1,23 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime;
+
+        try {
+            System.out.print("Enter an alarm time (HH:MM::SS): ");
+            String inputTime = scanner.nextLine();
+            alarmTime = LocalTime.parse(inputTime, formatter);
+            System.out.println("Alarm set for " + alarmTime);
+        } catch (DateTimeParseException e) {
+            System.out.println("Invalid format, please use HH:MM:SS");
+        }
+        scanner.close();
     }
 }
