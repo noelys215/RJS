23588c90dad3e5cc2fd774c08fe0ac3f36902a5b
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-05 18:50:26 -0500

    chore: while loop

--- alarm/src/Main.java
+++ alarm/src/Main.java
@@ -8,15 +8,17 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime;
+        LocalTime alarmTime = null;
 
-        try {
-            System.out.print("Enter an alarm time (HH:MM::SS): ");
-            String inputTime = scanner.nextLine();
-            alarmTime = LocalTime.parse(inputTime, formatter);
-            System.out.println("Alarm set for " + alarmTime);
-        } catch (DateTimeParseException e) {
-            System.out.println("Invalid format, please use HH:MM:SS");
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM::SS): ");
+                String inputTime = scanner.nextLine();
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format, please use HH:MM:SS");
+            }
         }
         scanner.close();
     }
