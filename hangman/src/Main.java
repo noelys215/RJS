312f93cf32349177feb3929b1860e1dd0b41e8d4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-08 19:21:02 -0500

    chore: add getHangmanArt method

--- hangman/src/Main.java
+++ hangman/src/Main.java
@@ -13,9 +13,53 @@ public class Main {
             wordState.add('_');
         }
 
-        System.out.println(wordState);
+        System.out.println("***********************");
+        System.out.println("Welcome to Java Hangman");
+        System.out.println("***********************");
+
+        System.out.println(getHangmanArt(5));
 
         scanner.close();
+    }
 
+    static String getHangmanArt(int wrongGuesses) {
+        return switch (wrongGuesses) {
+            case 0 -> """
+                                        
+                                        
+                                        
+                    """;
+            case 1 -> """
+                     o
+                                        
+                                        
+                    """;
+            case 2 -> """
+                     o
+                     |
+                                        
+                    """;
+            case 3 -> """
+                     o
+                    /|
+                                        
+                    """;
+            case 4 -> """
+                     o
+                    /|\\
+                                        
+                    """;
+            case 5 -> """
+                     o
+                    /|\\
+                    /
+                    """;
+            case 6 -> """
+                     o
+                    /|\\
+                    / \\
+                    """;
+            default -> "";
+        };
     }
 }
