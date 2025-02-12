0e09aa35d8b2244c9a5d72b1bf7af1f06b3ccf10
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-11 20:40:52 -0500

    chore: start dice

---
+++ timer/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        scanner.close();
+    }
+}
