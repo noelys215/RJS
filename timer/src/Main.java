3b8462165d505d9024759a7defe9b86bca5fcdcb
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-12 18:02:21 -0500

    chore: roll logic

--- timer/src/Main.java
+++ timer/src/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        int numOfDice;
+        int total = 0;
+
+        System.out.print("Enter the number of dice to roll: ");
+        numOfDice = scanner.nextInt();
+
+        if (numOfDice > 0) {
+            for (int i = 0; i < numOfDice; i++) {
+                int roll = random.nextInt(1, 7);
+                System.out.println("You rolled: " + roll);
+                total += roll;
+            }
+            System.out.println("Total: " + total);
+        } else {
+            System.out.println("Number of dice must be greater than 0");
+        }
 
         scanner.close();
     }
