44bdad1eb2a927d670959517c08ecd2eb253a954
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-14 18:00:35 -0500

    chore: complete dice game

--- timer/src/Main.java
+++ timer/src/Main.java
@@ -3,88 +3,75 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int numOfDice;
-        int total = 0;
+        var scanner = new Scanner(System.in);
+        var random = new Random();
 
         System.out.print("Enter the number of dice to roll: ");
-        numOfDice = scanner.nextInt();
+        int numOfDice = scanner.nextInt();
 
-        if (numOfDice > 0) {
-            for (int i = 0; i < numOfDice; i++) {
-                int roll = random.nextInt(1, 7);
-                printDie(roll);
-                System.out.println("You rolled: " + roll);
-                total += roll;
-            }
-            System.out.println("Total: " + total);
-        } else {
+        if (numOfDice <= 0) {
             System.out.println("Number of dice must be greater than 0");
+            return;
         }
 
+        int total = 0;
+        for (int i = 0; i < numOfDice; i++) {
+            int roll = random.nextInt(1, 7);
+            printDie(roll);
+            System.out.println("You rolled: " + roll);
+            total += roll;
+        }
+
+        System.out.println("Total: " + total);
         scanner.close();
     }
 
     static void printDie(int roll) {
+        String[] diceFaces = {
+                """
+             -------
+            |       |
+            |   ●   |
+            |       |
+             -------
+            """,
+                """
+             -------
+            | ●     |
+            |       |
+            |     ● |
+             -------
+            """,
+                """
+             -------
+            | ●     |
+            |   ●   |
+            |     ● |
+             -------
+            """,
+                """
+             -------
+            | ●   ● |
+            |       |
+            | ●   ● |
+             -------
+            """,
+                """
+             -------
+            | ●   ● |
+            |   ●   |
+            | ●   ● |
+             -------
+            """,
+                """
+             -------
+            | ●   ● |
+            | ●   ● |
+            | ●   ● |
+             -------
+            """
+        };
 
-        String dice1 = """
-                 -------
-                |       |
-                |   ●   |
-                |       |
-                 -------
-                """;
-
-        String dice2 = """
-                 -------
-                | ●     |
-                |       |
-                |     ● |
-                 -------
-                """;
-
-        String dice3 = """
-                 -------
-                | ●     |
-                |   ●   |
-                |     ● |
-                 -------
-                """;
-
-        String dice4 = """
-                 -------
-                | ●   ● |
-                |       |
-                | ●   ● |
-                 -------
-                """;
-
-        String dice5 = """
-                 -------
-                | ●   ● |
-                |   ●   |
-                | ●   ● |
-                 -------
-                """;
-
-        String dice6 = """
-                 -------
-                | ●   ● |
-                | ●   ● |
-                | ●   ● |
-                 -------
-                """;
-
-        switch (roll) {
-            case 1 -> System.out.print(dice1);
-            case 2 -> System.out.print(dice2);
-            case 3 -> System.out.print(dice3);
-            case 4 -> System.out.print(dice4);
-            case 5 -> System.out.print(dice5);
-            case 6 -> System.out.print(dice6);
-            default -> System.out.print("Invalid roll");
-        }
+        System.out.print(diceFaces[roll - 1]);
     }
 }
