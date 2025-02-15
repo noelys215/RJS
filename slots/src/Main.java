aaf6c1daa3d7b4bf2bf58cbaafcf85159b2b9c3c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-15 17:22:34 -0500

    feat: start slots

---
+++ slots/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        scanner.close();
+    }
+}
