fed0067d906c0bafacfc443377416241dab394e4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-16 12:43:19 -0500

    chore: add symbols

--- slots/src/Main.java
+++ slots/src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int balance = 100;
         int bet;
         int payout;
         String[] row;
+
+        System.out.println("*************************");
+        System.out.println("  Welcome to Java Slots  ");
+        System.out.println("Symbols: 🍒 🍉 🍋 🔔 ⭐ ");
+        System.out.println("*************************");
 
         scanner.close();
     }
